89d54932b66ebf0bde62cb60d33e586c4754260c
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-08-26 19:00:44 +0800

    Add Options panel

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Mixer")]
+    public AudioMixer audioMixer;
+
+    [Header("Audio Sources")]
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    private bool isMuted = false;
+    private float lastMusicVolume = 1f;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    //public void StopMusic()
+    //{
+    //    musicSource.Stop();
+    //}
+
+    //public void PlaySFX(AudioClip clip)
+    //{
+    //    sfxSource.PlayOneShot(clip);
+    //}
+
+    public void SetMusicVolume(float volume)
+    {
+        
+        if (isMuted)
+        {
+            lastMusicVolume = volume;
+            return;
+        }
+         
+        lastMusicVolume = volume;
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f);
+    }
+
+    //public void SetSFXVolume(float volume)
+    //{
+    //    audioMixer.SetFloat("SFXVolume", Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1f)) * 20f);
+    //}
+
+    public void ToggleMute(bool mute)
+    {
+        isMuted = mute;
+
+        if (mute) audioMixer.SetFloat("MusicVolume", -80f);
+        else SetMusicVolume(lastMusicVolume);
+    }
+
+    public float GetMusicVolume() => lastMusicVolume;
+    public bool IsMusicMuted() => isMuted;
+}
