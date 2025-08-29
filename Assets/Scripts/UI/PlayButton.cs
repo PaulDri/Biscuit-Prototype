a947b16bf2155289f850679f9ee334ce3c7417da
thisellie <81550623+arnplsrz@users.noreply.github.com>
2025-08-29 11:42:25 +0800

    Add play button animation

---
+++ Assets/Scripts/UI/PlayButton.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class PlayButton : MonoBehaviour
+{
+    Button playButton;
+    CanvasGroup buttonCanvas;
+    public CanvasGroup screenFader;
+    public AudioClip playSound;
+    public string nextSceneName = "GameScene";
+
+    void Start()
+    {
+        playButton = GetComponent<Button>();
+        buttonCanvas = GetComponent<CanvasGroup>();
+        playButton.onClick.AddListener(OnPlayClicked);
+    }
+
+    void OnPlayClicked()
+    {
+        playButton.interactable = false;
+
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(
+            buttonCanvas.DOFade(0f, 0.1f)
+            .OnStart(() => AudioManager.Instance.PlaySFX(playSound))
+        );
+        
+        seq.Append(buttonCanvas.DOFade(1f, 0.1f));
+        seq.Append(buttonCanvas.DOFade(0f, 0.1f));
+        seq.Append(buttonCanvas.DOFade(1f, 0.1f));
+
+        seq.Append(screenFader.DOFade(1f, 1.5f));
+
+        seq.AppendInterval(1f);
+        seq.OnComplete(() => SceneManager.LoadScene(nextSceneName));
+    }
+}
